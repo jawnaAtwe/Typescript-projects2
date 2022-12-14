7be4ee908b6f661b1e510317c2e5b976ab26b969
JawnaAtwe <jawnaamjad@gmail.com>
2022-12-14 14:00:52 +0200

    load more

--- src/Component/main.tsx
+++ src/Component/main.tsx
@@ -15,19 +15,21 @@ const main = () => {
   const [inputValue, setInputValue] = useState('');
   const [original, setOriginal] = useState<Movies[]>([]);
   const [wish, setWish] = useState<Movies[]>([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [listToShow, setListToShow] = useState<Movies[]>([]);
 
   const toWish = (movies: Movies) => {
     const arr: Movies[] = [movies];
     const arr1: Movies[] = [...wish, ...arr];
     setWish(arr1);
-    const res = original.filter((obj) => obj.id !== movies.id);
-    setOriginal(res);
+    const res = listToShow.filter((obj) => obj.id !== movies.id);
+    setListToShow(res);
   };
 
   const toList = (movies: Movies) => {
     const arr: Movies[] = [movies];
-    const arr1: Movies[] = original.concat(arr);
-    setOriginal(arr1);
+    const arr1: Movies[] = listToShow.concat(arr);
+    setListToShow(arr1);
     const res = wish.filter((obj) => obj.id !== movies.id);
     setWish(res);
   };
@@ -36,20 +38,31 @@ const main = () => {
     setInputValue(e.target.value);
   };
 
+  const api = async () => {
+    const data = await fetch('https://mocki.io/v1/907dd10c-32f7-48a2-a56f-89c66acb723d', {
+      method: 'GET'
+    });
+    const jsonData = await data.json();
+    setOriginal(jsonData.data);
+    setPageNumber(10);
+    if (!data.ok) {
+      const msg = `res:${data.status}`;
+      throw new Error(msg);
+    } else {
+      console.log('ok');
+    }
+  };
+
+  const loadMore = () => {
+    setPageNumber(pageNumber + 10);
+  };
+
   useEffect(() => {
-    const api = async () => {
-      const data = await fetch('https://mocki.io/v1/907dd10c-32f7-48a2-a56f-89c66acb723d', {
-        method: 'GET'
-      });
-      const jsonData = await data.json();
-      setOriginal(jsonData.data);
-      if (!data.ok) {
-        const msg = `res:${data.status}`;
-        throw new Error(msg);
-      } else {
-        console.log('ok');
-      }
-    };
+    const arrfin = listToShow.concat(original.slice(pageNumber - 10, pageNumber));
+    setListToShow(arrfin);
+  }, [pageNumber]);
+
+  useEffect(() => {
     api();
   }, []);
 
@@ -58,10 +71,9 @@ const main = () => {
       <div className="input-container">
         <input type={'text'} placeholder={'Enter'} onChange={onInputChange} />
       </div>
-
       <div className="Mainlist">
         <ListComponent
-          list={original}
+          list={listToShow}
           buttonText={'go to wish'}
           callback={toWish}
           inputValue={inputValue}
@@ -72,7 +84,10 @@ const main = () => {
           callback={toList}
           inputValue={inputValue}
         />
-      </div>
+      </div>{' '}
+      <button type={'submit'} onClick={loadMore}>
+        Load More
+      </button>
     </div>
   );
 };
